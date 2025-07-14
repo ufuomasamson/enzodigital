40b6312abe931ca21c242e44a50991d7ddbbe6ef
enzozone222 <samsonenzo@proton.me>
2025-07-14 09:23:54 -0700

    Fix: remove favicon, duplicate spinner, and modal video bug. Clean up particle background markup.

--- js/main.js
+++ js/main.js
@@ -75,13 +75,14 @@
     $('.btn-play').click(function () {
         $videoSrc = $(this).data("src");
     });
-    console.log($videoSrc);
     $('#videoModal').on('shown.bs.modal', function (e) {
-        $("#video").attr('src', $videoSrc + "?autoplay=1&amp;modestbranding=1&amp;showinfo=0");
-    })
+        if ($videoSrc) {
+            $("#video").attr('src', $videoSrc + "?autoplay=1&modestbranding=1&showinfo=0&mute=1");
+        }
+    });
     $('#videoModal').on('hide.bs.modal', function (e) {
-        $("#video").attr('src', $videoSrc);
-    })
+        $("#video").attr('src', "");
+    });
 
 
     // Facts counter
